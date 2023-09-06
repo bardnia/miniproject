df5c4a9c423d389267b49e3791fb4c1cbcbb1f46
bardnia <69304793+bardnia@users.noreply.github.com>
2023-09-07 05:39:04 +0900

    Delete first project directory

--- first project/js/main.js
+++
@@ -1,9 +0,0 @@
-const $page1 = document.querySelector('.page1');
-const $page2 = document.querySelector('.page2');
-const $page3 = document.querySelector('.page3');
-const $page4 = document.querySelector('.page4');
-
-$page1.onclick = function() { window.location.href="../first project/html/webtoon.html"}
-$page2.onclick = function() { window.location.href="../first project/html/webtext.html"}
-$page3.onclick = function() { window.location.href="../first project/html/movie.html"}
-$page4.onclick = function() { window.location.href="../first project/html/drama.html"}
