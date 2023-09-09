736fc6df38df81f9a42c1d56144a9a3aa732d675
bardnia <69304793+bardnia@users.noreply.github.com>
2023-09-09 11:30:06 +0900

    Add files via upload

--- js/tema.js
+++ js/tema.js
@@ -1,4 +1,7 @@
 const $temaget = document.querySelector('.tema');
+const $romance = document.querySelector('.romance');
+
 const pagetema = window.localStorage.getItem('tema');
 
 pagetema.onclick = function() { window.localStorage.setItem('tema','웹툰')}
+$romance.onclick = function() { window.localStorage.setItem('tema','웹툰')}
